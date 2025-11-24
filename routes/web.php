e76dc16391c595525347f66bb7540e7b0698f866
zanshere <fauzan224321@gmail.com>
2025-11-24 10:27:02 +0700

    feat(routes): add admin and auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.index');
 });
+
+// Admin Routes
+Route::get('/dashboard', function() {
+    return view('admin.index');
+});
+
+// Auth Routes
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::get('/forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
